c8e409f21663262e8a0edbc6c988e493f2b2be0e
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2023-05-23 21:21:36 -0400

    Add design system view

--- packages/client/src/providers/AppProvider.tsx
+++ packages/client/src/providers/AppProvider.tsx
@@ -1,7 +1,4 @@
-import { Button } from '@root/ui/common/Button';
-import { TextInput } from '@root/ui/forms/TextInput/TextInput';
-import { H1, P, View } from '@universal-labs/primitives';
-import { useForm } from 'react-hook-form';
+import { View } from '@universal-labs/primitives';
 
 interface IAppProviderProps {
   children: React.ReactNode;
@@ -9,26 +6,5 @@ interface IAppProviderProps {
 
 export function AppProvider({ children }: IAppProviderProps) {
   // TODO: Add React Query Provider
-  const form = useForm({
-    defaultValues: {
-      name: '',
-    },
-  });
-
-  return (
-    <View className='m-5 flex-1'>
-      <H1>Hello Gentleman Community!!</H1>
-      <TextInput label='Name' className='my-4' control={form.control} formField='name' />
-      <Button>
-        <P>Text Button</P>
-      </Button>
-      <Button variant='contained'>
-        <P>Contained Button</P>
-      </Button>
-      <Button variant='outlined'>
-        <P>Outlined Button</P>
-      </Button>
-      {children}
-    </View>
-  );
+  return <View className='m-5 flex-1'>{children}</View>;
 }
