d67e0a225eee96764ae894567eacaddb72d0cad0
Manki <damianorzusa2016@gmail.com>
2023-05-30 14:23:55 -0300

    move user schema validator to @root/shared

--- packages/server/src/routes/auth.route.ts
+++ packages/server/src/routes/auth.route.ts
@@ -1,26 +1,22 @@
 import { TRPCError } from '@trpc/server';
-import { z } from 'zod';
+import { userSchema } from '../../../shared/src/constants';
 import { publicProcedure, router } from '../trpc';
 
 export const authRouter = router({
-  login: publicProcedure
-    .input(z.object({ email: z.string().email(), password: z.string() }))
-    .mutation(async ({ input, ctx }) => {
-      const { data, error } = await ctx.supabase.auth.signInWithPassword(input);
-      return { data, error };
-    }),
+  login: publicProcedure.input(userSchema).mutation(async ({ input, ctx }) => {
+    const { data, error } = await ctx.supabase.auth.signInWithPassword(input);
+    return { data, error };
+  }),
 
   logout: publicProcedure.mutation(async ({ ctx }) => ctx.supabase.auth.signOut()),
 
-  singup: publicProcedure
-    .input(z.object({ email: z.string().email(), password: z.string() }))
-    .mutation(async ({ input, ctx }) => {
-      const { data, error } = await ctx.supabase.auth.signUp(input);
-      if (data.user && data.user.email) {
-        await ctx.supabase.auth.signInWithOtp({ email: data.user.email });
-      } else {
-        throw new TRPCError({ message: 'Something went wrong', code: 'BAD_REQUEST' });
-      }
-      return { data, error };
-    }),
+  singup: publicProcedure.input(userSchema).mutation(async ({ input, ctx }) => {
+    const { data, error } = await ctx.supabase.auth.signUp(input);
+    if (data.user && data.user.email) {
+      await ctx.supabase.auth.signInWithOtp({ email: data.user.email });
+    } else {
+      throw new TRPCError({ message: 'Something went wrong', code: 'BAD_REQUEST' });
+    }
+    return { data, error };
+  }),
 });
