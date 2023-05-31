9f2f0725a19bbb00040a7844514b9477f708de65
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2023-05-30 20:04:28 -0400

    add icons sacaffold

---
+++ packages/ui/src/icons/BackArrowIcon.tsx
@@ -0,0 +1,5 @@
+import Svg, { Path, type SvgProps } from 'react-native-svg';
+
+export function BackArrowIcon(props: SvgProps) {
+  return <Svg {...props}>{/*content*/}</Svg>;
+}
---
+++ packages/ui/src/icons/EyeIcon.tsx
@@ -0,0 +1,5 @@
+import Svg, { Path, type SvgProps } from 'react-native-svg';
+
+export function EyeIcon(props: SvgProps) {
+  return <Svg {...props}>{/*content*/}</Svg>;
+}
