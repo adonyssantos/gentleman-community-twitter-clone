30b643bb9834c2961f702502ccbee875e7f68bcd
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2023-06-06 22:35:52 -0400

    add ImageComponent

---
+++ packages/ui/src/images/ImageComponent.tsx
@@ -0,0 +1,6 @@
+// PROPS
+// icon: Component
+// size?: 'small' | 'medium' | 'large'
+// color?: 'primary' | 'secondary' | 'inherit' | 'action' | 'disabled' | 'error'
+// className?: string
+// style?: React.CSSProperties
