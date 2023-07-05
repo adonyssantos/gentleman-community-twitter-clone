bb267f6a899702ed991df4c49d8e2ad08e380109
Acxel <acxelandres@outlook.com>
2023-07-04 22:50:25 -0500

    calendar first commit

---
+++ apps/web/src/pages/test.tsx
@@ -0,0 +1,10 @@
+import { CalendarView } from '@root/ui/forms/DatePicker/CalendarView';
+import { View } from '@universal-labs/primitives';
+
+export default function test() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <CalendarView />
+    </View>
+  );
+}
---
+++ packages/ui/src/forms/DatePicker/styles.ts
@@ -0,0 +1,6 @@
+import { cva } from 'class-variance-authority';
+
+export const datePicker = cva('', {
+  variants: {},
+  defaultVariants: {},
+});
