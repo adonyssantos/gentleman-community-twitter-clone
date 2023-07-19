71edfd86166a847e21e79ae9c9515870acbaec07
0s1n <jpablo_166@hotmail.com>
2023-07-18 20:28:26 -0500

    Fix: @adonyssantos

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,16 +1,1 @@
-// export { trpc } from './trpc';
-
-// // Providers
-// export { AppProvider } from './providers/AppProvider';
-
-// // Hooks
-// export { usePasswordReset } from './hooks/usePasswordReset';
-export { useSignIn } from './hooks/useSignIn';
-// export { useSignUp } from './hooks/useSignUp';
-
-// // Views
-// export { PasswordResetView } from './views/password_reset/PasswordReset';
-// export { SignInView } from './views/signin/SignIn';
-// export { SignUpView } from './views/signup/SignUp';
-
 export {};
