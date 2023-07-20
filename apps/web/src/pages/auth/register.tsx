3d866fb2fceebac1c37a946300d1ae2585eab0c8
Manki <damianorzusa2016@gmail.com>
2023-07-19 23:38:14 -0300

    connect public.users and auth.users

--- apps/web/src/pages/auth/register.tsx
+++ apps/web/src/pages/auth/register.tsx
@@ -4,20 +4,26 @@ import { trpc } from '@root/client/trpc';
 const Register = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [name, setName] = useState<string>('');
 
   const singupMutation = trpc.auth.singup.useMutation();
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const res = await singupMutation.mutateAsync({ email, password });
+    const res = await singupMutation.mutateAsync({ name, email, password });
     console.log(res);
   };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input type='text' placeholder='email' onChange={(e) => setEmail(e.target.value)} />
-        <input type='password' onChange={(e) => setPassword(e.target.value)} />
+        <input type='text' placeholder='name' onChange={(e) => setName(e.target.value)} />
+        <input type='email' placeholder='email' onChange={(e) => setEmail(e.target.value)} />
+        <input
+          type='password'
+          placeholder='password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <input type='submit' />
       </form>
     </div>
