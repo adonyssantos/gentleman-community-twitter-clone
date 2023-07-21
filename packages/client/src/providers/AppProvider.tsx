0dea08500cc1a815e876057c3b48695fa5ebe9dc
Manki <damianorzusa2016@gmail.com>
2023-07-21 20:59:40 -0300

    =refactor

--- packages/client/src/providers/AppProvider.tsx
+++ packages/client/src/providers/AppProvider.tsx
@@ -1,10 +1,11 @@
+import { AppProps } from 'next/app';
 import { AuthProvider } from '../context/auth.context';
 import TrpcProvider from '../trpc/trpc.provider';
 import { TranslationsProvider } from './TranslationsProvider';
 
 export interface AppProviderProps {
   children: React.ReactNode;
-  pageProps: any;
+  pageProps: AppProps['pageProps'];
 }
 
 export function AppProvider({ pageProps, children }: AppProviderProps) {
