fb58a220177366b9cf9bc083086d8f7faee7666e
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2023-07-21 19:25:09 -0400

    initial

---
+++ packages/client/src/views/getting_started/profilepicture/index.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@root/ui/common/Typography';
+import { View } from '@universal-labs/primitives';
+
+export function ProfilePictureView() {
+  return (
+    <View className='m-auto max-w-lg flex-1 p-5'>
+      <Typography variant='h1'>Profile Picture</Typography>
+    </View>
+  );
+}
