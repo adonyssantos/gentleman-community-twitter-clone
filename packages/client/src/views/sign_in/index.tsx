7cf03102d92b83047a555e97994172eab6c42ea9
0s1n <jpablo_166@hotmail.com>
2023-07-24 20:18:11 -0500

    Fix

--- packages/client/src/views/sign_in/index.tsx
+++ packages/client/src/views/sign_in/index.tsx
@@ -6,7 +6,8 @@ import { TextInput } from '@root/ui/forms/TextInput';
 import { BrandGithubIcon } from '@root/ui/icons/BrandGithubIcon';
 import { BrandGoogleIcon } from '@root/ui/icons/BrandGoogleIcon';
 import { ColorLogo } from '@root/ui/images/Logo/ColorLogo';
-import { P, View } from '@universal-labs/primitives';
+import { View } from '@universal-labs/primitives';
+import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 
 export function SignInView() {
@@ -19,44 +20,49 @@ export function SignInView() {
   });
 
   return (
-    <Container className=''>
-      <View className='m-auto max-h-full max-w-lg flex-1 items-center justify-center p-5'>
+    <Container className='flex min-h-screen justify-between'>
+      <View className='items-center p-4'>
         <ColorLogo size='small' />
       </View>
-      <View className='mt-10 flex h-full flex-1 items-center justify-center'>
-        <View className='flex flex-col space-y-6 p-4'>
-          <Typography variant='h4' className='!font-bold'>
-            {t('title')}
-          </Typography>
-          <Button variant='outlined' startIcon={<BrandGoogleIcon />}>
-            {t('with_google')}
-          </Button>
-          <Button variant='outlined' startIcon={<BrandGithubIcon />}>
-            {t('with_gitHub')}
-          </Button>
-          <Typography variant='subtitle1' className='flex w-full flex-row'>
-            <P className='my-2 h-0.5 flex-1 self-center bg-slate-400' />
-            <P className='flex justify-center px-4'>{t('or')}</P>
-            <P className='my-2 h-0.5 flex-1 self-center bg-slate-400' />
-          </Typography>
-          <TextInput
-            label={t('alternative')}
-            className='my-4'
-            control={form.control}
-            formField='alternative'
-          />
-          <Button variant='contained'>{t('next')}</Button>
-          <Button variant='outlined'>{t('forgot_password')}</Button>
-          <View className='flex flex-row gap-1 py-8'>
-            <Typography variant='subtitle1' className='text-slate-700'>
-              {t('do_not_have_an_account')}
-            </Typography>
-            <Typography variant='subtitle1' color={'primary'}>
-              {t('sign_up')}
-            </Typography>
+      <View className='flex flex-col space-y-5'>
+        <Typography variant='h4' className='!font-bold '>
+          {t('title')}
+        </Typography>
+        <View className='flex flex-col'>
+          <View className='flex flex-col gap-2'>
+            <Button variant='outlined' startIcon={<BrandGoogleIcon />}>
+              {t('with_google')}
+            </Button>
+            <Button variant='outlined' startIcon={<BrandGithubIcon />}>
+              {t('with_gitHub')}
+            </Button>
+          </View>
+          <View className='mx-1 my-2 flex flex-row items-center'>
+            <View className='h-0.5 flex-1 bg-gray-400' />
+            <Typography className='flex px-4'>{t('or')}</Typography>
+            <View className=' h-0.5 flex-1 bg-gray-400' />
+          </View>
+          <View className='flex flex-col gap-2'>
+            <TextInput
+              label={t('alternative')}
+              className=''
+              control={form.control}
+              formField='alternative'
+            />
+            <Button variant='contained'>{t('next')}</Button>
+            <Button variant='outlined'>{t('forgot_password')}</Button>
           </View>
         </View>
+        <View className='flex flex-row gap-1 pt-4'>
+          <Typography variant='subtitle1' className='text-slate-700'>
+            {t('do_not_have_an_account')}
+            <Link href={'sign_up'} className='text-primary-500'>
+              {t('sign_up')}
+            </Link>
+          </Typography>
+        </View>
       </View>
+      <View className='' />
     </Container>
   );
 }
--- packages/client/src/views/sign_up/index.tsx
+++ packages/client/src/views/sign_up/index.tsx
@@ -4,15 +4,15 @@ import { Button } from '@root/ui/common/Button';
 import { Typography } from '@root/ui/common/Typography';
 import { BrandGithubIcon } from '@root/ui/icons/BrandGithubIcon';
 import { BrandGoogleIcon } from '@root/ui/icons/BrandGoogleIcon';
-import { P, View } from '@universal-labs/primitives';
+import { View } from '@universal-labs/primitives';
 import Link from 'next/link';
 
 export function SignUpView() {
   const { t } = useI18n(['signup']);
 
   return (
-    <Container className='mt-10 flex h-full flex-1 items-center justify-center'>
-      <View className='flex flex-col space-y-6 p-4'>
+    <Container className='flex min-h-screen justify-center'>
+      <View className='flex flex-col'>
         <Typography variant='h4' className='!font-bold'>
           {t('title')}
         </Typography>
@@ -22,13 +22,13 @@ export function SignUpView() {
         <Button variant='outlined' startIcon={<BrandGithubIcon />}>
           {t('with_gitHub')}
         </Button>
-        <Typography variant='subtitle1' className='flex w-full flex-row'>
-          <P className='my-2 h-0.5 flex-1 self-center bg-slate-400' />
-          <P className='flex justify-center px-4'>{t('or')}</P>
-          <P className='my-2 h-0.5 flex-1 self-center bg-slate-400' />
-        </Typography>
+        <View className='mx-1 my-2 flex flex-row items-center'>
+          <View className='h-0.5 flex-1 bg-slate-400' />
+          <Typography className='flex px-4'>{t('or')}</Typography>
+          <View className='h-0.5 flex-1 bg-slate-400' />
+        </View>
         <Button variant='contained'>{t('create_account')}</Button>
-        <View className='flex flex-row justify-center'>
+        <View className='flex flex-row justify-center p-1'>
           <Typography variant='caption' className=''>
             {t('agree')}
             <Typography variant='caption' color={'primary'}>
@@ -46,7 +46,7 @@ export function SignUpView() {
         </View>
         <Typography variant='subtitle1'>
           {t('already')}
-          <Link href={'login'} className='text-primary-500'>
+          <Link href={'sign_in'} className='text-primary-500'>
             {t('log_in')}
           </Link>
         </Typography>
