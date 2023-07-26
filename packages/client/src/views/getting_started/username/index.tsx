7e84f4fd2d7b7ae8419d428480ea079ac9df453c
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2023-07-26 19:14:04 -0400

    add username view

---
+++ packages/client/src/views/getting_started/username/index.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@root/ui/common/Typography';
+import { View } from '@universal-labs/primitives';
+
+export function UsernameView() {
+  return (
+    <View className='m-auto max-w-lg flex-1 p-5'>
+      <Typography variant='h1'>Username view</Typography>
+    </View>
+  );
+}
