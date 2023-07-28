373f9bd8ce66e242e60078f3790d1999a2b09f8a
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2023-07-28 00:34:19 -0400

    extends ContainerProps interface from ViewProps

--- packages/client/src/components/Container/index.tsx
+++ packages/client/src/components/Container/index.tsx
@@ -1,11 +1,16 @@
 import type { ReactNode } from 'react';
+import type { ViewProps } from 'react-native';
 import { View } from '@universal-labs/primitives';
 import clsx from 'clsx';
 
-export interface ContainerProps {
+export interface ContainerProps extends ViewProps {
   children: ReactNode;
   className: string;
 }
-export function Container({ children, className }: ContainerProps) {
-  return <View className={clsx('m-auto max-w-lg flex-1 p-5', className)}>{children}</View>;
+export function Container({ children, className, ...props }: ContainerProps) {
+  return (
+    <View className={clsx('m-auto max-w-lg flex-1 p-5', className)} {...props}>
+      {children}
+    </View>
+  );
 }
