e7545ce4a565d81a34e905cdfa9f89f878b1a538
Manki <damianorzusa2016@gmail.com>
2023-08-03 00:51:22 -0300

    lint error solved

--- packages/shared/src/validators/user.model.ts
+++ packages/shared/src/validators/user.model.ts
@@ -11,15 +11,4 @@ export const loginSchema = z.object({
   password: z.string(),
 });
 
-export const singupSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string(),
-});
-
-export const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
-});
-
 export const validateSchema = z.object({ username: z.string() });
