2471accb5e2a49e999c6293da094a94f861ff51a
David León <86429650+jdavidleon@users.noreply.github.com>
2023-08-09 05:12:03 +0000

    here2

--- apps/web/src/pages/auth/index.tsx
+++ apps/web/src/pages/auth/index.tsx
@@ -3,8 +3,10 @@ import { useAuthContext } from '@root/client/context/auth.context';
 
 const Index = () => {
   const auth = useAuthContext();
-
   const [email, setImail] = useState('');
+  const [emailSingup, setEmailSignup] = useState('');
+  const [passwordSingup, setPasswordSingup] = useState('');
+  const [name, setName] = useState('');
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -16,7 +18,8 @@ const Index = () => {
 
   const onSingup = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const result = await auth?.({ email });
+    // { email: emailSingup, password: passwordSingup }
+    const result = await auth?.singup({ email: emailSingup, name, password: passwordSingup });
     const { data, error } = result ?? {}; // Set data and error to {} (empty object) if result is undefined or null
 
     console.log(data, error);
@@ -30,7 +33,20 @@ const Index = () => {
       </form>
       <br />
       <form onSubmit={onSingup}>
-        <input type='email' onChange={(e) => setImail(e.target.value)} />
+        <input
+          type='email'
+          placeholder='email'
+          onChange={(e) => setEmailSignup(e.target.value)}
+        />
+        <br />
+        <input
+          type='text'
+          placeholder='password'
+          onChange={(e) => setPasswordSingup(e.target.value)}
+        />
+        <br />
+        <input type='text' placeholder='name' onChange={(e) => setName(e.target.value)} />
+        <br />
         <input type='submit' value='submit' />
       </form>
     </div>
